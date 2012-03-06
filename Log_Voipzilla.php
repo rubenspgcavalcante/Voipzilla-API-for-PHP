92ffd474750c75b674cd765a8bb978f35522c3fa
Rubens Pinheiro <rubens.pinheiro@argohost.net>
2012-03-06 16:46:04 -0300

    Log class added

---
+++ Log_Voipzilla.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * Log class to Voipzilla api
+ * 
+ * @author Mob Dev Team
+ * @version 0.1 
+ * @package voipzilla-socket
+ * 
+ */
+
+class Log_Voipzilla{
+
+	function __construct(){
+
+
+	}
+
+	function __destruct(){
+
+
+	}
+
+	static function save(){
+
+
+	}
+
+
+}
